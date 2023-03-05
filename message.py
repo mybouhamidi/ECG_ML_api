a42f676478f9f3370100a47cadd19141568f931a
Yassine1231 <mbouh050@uottawa.ca>
2023-03-05 17:27:25 -0500

    Changed ML model

--- message.py
+++ message.py
@@ -1,88 +1,31 @@
-from fastapi import Request, FastAPI
-
-# from typing import List
+from fastapi import FastAPI
+from typing import List
 import numpy as np
-import torch
-import torch.nn as nn
 
-# from fastapi.encoders import jsonable_encoder
-from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
 
-# import json
-
-
-# class Item(BaseModel):
-#     data: List[float]
-
+from pydantic import BaseModel
+from tensorflow import keras
 
 app = FastAPI()
 
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+from pydantic import BaseModel
 
 
-def nin_block(in_channels, out_channels, kernel_size, padding, strides):
-    return nn.Sequential(
-        nn.Conv1d(in_channels, out_channels, kernel_size, strides, padding),
-        nn.BatchNorm1d(out_channels),
-        nn.GELU(),
-        nn.Conv1d(out_channels, out_channels, kernel_size=1),
-        nn.GELU(),
-        nn.Conv1d(out_channels, out_channels, kernel_size=1),
-        nn.GELU(),
-    )
+class Item(BaseModel):
+    data: List[float]
 
 
-def get_model():
-    return nn.Sequential(  # input shape: (batch_size, 1, 187)
-        nin_block(
-            1, 48, kernel_size=11, strides=4, padding=0
-        ),  # output shape: (batch_size, 48, 44)
-        nn.MaxPool1d(3, stride=2),  # output shape: (batch_size, 48, 21)
-        nin_block(
-            48, 128, kernel_size=5, strides=1, padding=2
-        ),  # output shape: (batch_size, 128, 21)
-        nn.MaxPool1d(3, stride=2),  # output shape: (batch_size, 128, 10)
-        nin_block(
-            128, 256, kernel_size=3, strides=1, padding=1
-        ),  # output shape: (batch_size, 256, 10)
-        nn.MaxPool1d(3, stride=2),  # output shape: (batch_size, 256, 4)
-        nn.Dropout(0.4),
-        # last layers for classification of 5 classes
-        nin_block(
-            256, 5, kernel_size=3, strides=1, padding=1
-        ),  # output shape: (batch_size, 5, 4)
-        nn.AdaptiveAvgPool1d(1),  # output shape: (batch_size, 5, 1)
-        nn.Flatten(),  # output shape: (batch_size, 5)
-    )
+model = keras.models.load_model("bestmodel")
+class_names = {0: "N", 1: "S", 2: "V", 3: "F", 4: "Q"}
 
 
 @app.post("/predict")
-async def classify(heartbeat: Request):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = get_model().to(device=device)
-    model.load_state_dict(torch.load("best_model0.99.pt"))
+async def classify(heartbeat: Item):
+    item = heartbeat.dict()
 
-    class_names = {0: "N", 1: "S", 2: "V", 3: "F", 4: "Q"}
-    # item = Item(heartbeat["data"])
-    item = await heartbeat.json()
-    # print(item)
-    # item = json.loads(item)
-    # print(item["data"])
+    predictions = model.predict(pd.DataFrame(item["data"]).transpose())
+    predictions = np.argmax(predictions, axis=1)
 
-    x = np.array(item["data"][:-2], dtype="f").reshape(1, 1, 187)
-
-    y = model(torch.tensor(x).to(device=device))
-    y = torch.argmax(y, dim=1).cpu().numpy()
-
-    i = y.tolist()
+    i = predictions.tolist()
     return {"result": class_names[i[0]]}
-    # return await heartbeat.json()
-
--- training/request.py
+++ training/request.py
@@ -1,28 +1,20 @@
 import requests
 import pandas as pd
 from time import time
-import numpy as np
+
 import json
 
-test_df = pd.read_csv("ML-data/mitbih_test.csv", header=None)
-features_df = test_df.iloc[:, :-1]
-features_df = features_df.astype("float32")
-X_test_np = features_df.to_numpy()
-X_test_np = np.reshape(X_test_np, (X_test_np.shape[0], 1, X_test_np.shape[1]))
-X_test_np = (X_test_np - X_test_np.mean()) / X_test_np.std()
+test_df = pd.read_csv("demo2.csv", delimiter=";", header=None)
+features_df = test_df.astype("float32").transpose()
 
 class_names = {0: "N", 1: "S", 2: "V", 3: "F", 4: "Q"}
-
-x = X_test_np[:1, :, :]
-x = x.reshape(1, 1, 187)
-
-w = list(x[0][0].tolist())
-
 
 # url = "https://ecg-ml-api.onrender.com/predict"
 url = "http://localhost:8000/predict"
 
+# print(features_df.to_dict(orient="list"))
 previous = time()
-y = requests.post(url, json={"data": w})
+y = requests.post(url, json={"data": features_df.to_dict(orient="list")[0]})
+
 w = json.loads(y.text)
 print(w["result"], time() - previous)
